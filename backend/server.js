b7cc68cc5a45b3c7149a86dba8d87c47aa673e81
prabhas-koppireddy <prabhaskoppireddy@gmail.com>
2026-06-11 11:15:48 +0530

    prepared for the deployment

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,29 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import path from "path";
+
 import notesRoutes from "./src/routes/notesRoutes.js"
 import { connectDB } from "./src/config/db.js";
-import dotenv from "dotenv";
 import rateLimiter from "./src/middleware/rateLimiter.js";
-import cors from "cors";
+
+
 
 
 dotenv.config();
 //console.log(process.env.MONGO_URL);
 const app = express();
 const port = process.env.PORT;
+const __dirname = path.resolve()
 
 // middleware
-app.use(cors({
-    origin: "http://localhost:5173"
-}));
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
 app.use(express.json()); // this middleware will parse JSON bodies - req.body
 app.use(rateLimiter);
 
@@ -26,6 +35,14 @@ app.use(rateLimiter);
 // });
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
     app.listen(5001, () => {
